4aaf0b6654ba389c9f9f2f7f44529e6ae5a5557b
Leandro <leandro.fonseca1621@gmail.com>
2024-06-01 12:40:44 -0300

    feat: atualizacoes

---
+++ src/components/Hero.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
+import Link from 'next/link';
+import Image from 'next/image';
+
+const Hero = () => {
+  return (
+    <div className='pb-10 pt-10'>
+
+
+    </div>
+    
+  )
+}
+
+
+
+export default Hero
