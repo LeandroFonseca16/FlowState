c301bbbab848a2d65b8aa91f52f7fbc87d6e87fc
Leandro <leandro.fonseca1621@gmail.com>
2024-06-03 00:07:13 -0300

    feat: atualizacoes de usabilidade

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 import Hero from "@/components/Hero";
-
 export default function Home() {
   return (
-    <main className="relative bg-gray-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
+    <main className="relative flex justify-center items-center flex-col overflow-hidden mx-auto">
+      <div className="w-full">
         <Hero />
       </div>
     </main>
---
+++ components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="flex justify-center w-screen">
+      <div className="w-screen bg-gray-100 h-20 flex items-center justify-center">
+        teste
+      </div>
+    </div>
+  );
+};
+
+export default Header;
